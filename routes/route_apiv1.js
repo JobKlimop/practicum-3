0f7ae875c6c5b4f19397d771b3bf10b0f039d160
Thomas Mout <stmout@avans.nl>
2017-05-11 14:16:50 +0200

    Alle countries opvragen

--- routes/route_apiv1.js
+++ routes/route_apiv1.js
@@ -9,16 +9,9 @@ var router = express.Router();
 //var connector = require('../db/db_connection.js');
 var pool = require('../db/db_connector.js');
 
-router.get('/actors/:lastname?', function (req, res, next) {
+router.get('/countries', function (req, res, next) {
 
-	var lastname = req.params.lastname;
-
-	var query_str;
-	if (lastname) {
-		query_str = 'SELECT * FROM actor_info WHERE last_name ="' + lastname + '";';
-	} else {
-		query_str = 'SELECT * FROM actor_info;';
-	}
+	query_str = 'SELECT * FROM country;';
 
 	pool.getConnection( function (err, connection) {
 		if(err){ throw error }
@@ -28,27 +21,6 @@ router.get('/actors/:lastname?', function (req, res, next) {
 			res.status(200).json(rows);
 		});
 	});
-});
-
-var users = [
-	{
-		name: 'diederich',
-		info: {
-			email: 'dm.kroeske@avans.nl',
-			nick: 'bug producer'
-		}
-	},
-	{
-		name: 'robin',
-		info: {
-			email: 'r.schellius@avans.nl',
-			nick: 'bug fixer'
-		}
-	}
-];
-
-router.get('/users', function (req, res) {
-	res.json(users);
 });
 
 router.get('/help', function (req, res) {
