d42c3cd33336172b099d16a2e6d621715066c66a
Thomas <thomas_641991@hotmail.com>
2017-05-11 14:10:08 +0200

    City added to apiv1

--- routes/route_apiv1.js
+++ routes/route_apiv1.js
@@ -6,18 +6,17 @@
 
 var express = require('express');
 var router = express.Router();
-//var connector = require('../db/db_connection.js');
 var pool = require('../db/db_connector.js');
 
-router.get('/actors/:lastname?', function (req, res, next) {
+router.get('/cities/:name?', function (req, res, next) {
 
-	var lastname = req.params.lastname;
+	var name = req.params.name;
 
 	var query_str;
-	if (lastname) {
-		query_str = 'SELECT * FROM actor_info WHERE last_name ="' + lastname + '";';
+	if (name) {
+		query_str = 'SELECT * FROM city WHERE name ="' + name + '";';
 	} else {
-		query_str = 'SELECT * FROM actor_info;';
+		query_str = 'SELECT * FROM city;';
 	}
 
 	pool.getConnection( function (err, connection) {
@@ -30,35 +29,8 @@ router.get('/actors/:lastname?', function (req, res, next) {
 	});
 });
 
-var users = [
-	{
-		name: 'diederich',
-		info: {
-			email: 'dm.kroeske@avans.nl',
-			nick: 'bug producer'
-		}
-	},
-	{
-		name: 'robin',
-		info: {
-			email: 'r.schellius@avans.nl',
-			nick: 'bug fixer'
-		}
-	}
-];
-
-router.get('/users', function (req, res) {
-	res.json(users);
-});
-
 router.get('/help', function (req, res) {
 	res.json({"msg": "Help function"});
-});
-
-router.get('*', function (req, res) {
-	res.json(
-		{"msg": "Thank you for using API v3"}
-	);
 });
 
 module.exports = router;
